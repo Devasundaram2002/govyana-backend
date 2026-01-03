eff676d4486aa7245cb60294767213f61e98a532
Devasundaram2002 <devasundaram247@gmail.com>
2026-01-03 20:32:11 +0530

    Added reset password API

--- src/main/java/com/deva/Govyana/controller/AuthController.java
+++ src/main/java/com/deva/Govyana/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.deva.Govyana.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.deva.Govyana.dto.LoginRequest;
+import com.deva.Govyana.dto.ResetPasswordRequest;
 import com.deva.Govyana.model.User;
+import com.deva.Govyana.repository.UserRepository;
 import com.deva.Govyana.service.UserService;
 
 @RestController
@@ -15,7 +20,14 @@ public class AuthController {
 
     @Autowired
     private UserService userService;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public AuthController(UserRepository userRepository,
+                          PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
     // REGISTER
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody User user) {
@@ -48,5 +60,23 @@ public class AuthController {
 
             return ResponseEntity.internalServerError().body("Server error");
         }
+    }
+    @PostMapping("/reset-password")
+    public ResponseEntity<?> resetPassword(@RequestBody ResetPasswordRequest request) {
+
+        Optional<User> optionalUser = userRepository.findByEmail(request.getEmail());
+
+        if (optionalUser.isEmpty()) {
+            return ResponseEntity.status(404).body("User not found");
+        }
+
+        User user = optionalUser.get();
+
+        // password encrypt panna best
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Password reset successful");
     }
 }
